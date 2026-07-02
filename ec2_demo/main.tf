8c3b14958b17488fbe9f6ba108e60f4b6ac93280
iopshub-gromo <archana.singh@iopshub@.com>
2026-07-02 21:46:23 +0530

    testing

--- ec2_demo/main.tf
+++ ec2_demo/main.tf
@@ -1,20 +1,21 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["099720109477"] # Canonical
 
   filter {
-    name   = ""
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
 resource "aws_instance" "demo" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro" 
+  instance_type = "t3.micro"
 
   tags = {
     Name = "EC2_Atla"
